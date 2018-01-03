7060bb29eaa29f9b70aea79d7afd1e32cce0e83a
kostarubtsov1990 <kostarubtsov@hotmail.com>
2018-01-03 18:10:58 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Background;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+
+public class Controller {
+    @FXML
+    private Button button;
+@FXML
+    protected void signIn() throws Exception{
+
+        //button.setEffect(new DropShadow());
+    }
+}
